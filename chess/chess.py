93089922dbdad86b034a557494f2435648514298
shuming li <shumingli@shumings-MacBook-Pro.local>
2017-04-16 21:44:24 -0700

    change k pos

--- chess/chess.py
+++ chess/chess.py
@@ -1,13 +1,14 @@
 from copy import deepcopy
+import pandas as pd
 class chess(object):
     def __init__(self):
         self.C = True
         self.p = None
 #         self.board = self.board()
-        self.pos_dict = {'r1':'a1','h1':'b1','b1':'c1','q1':'d1','k1':'e1','b2':'f1','h2':'g1','r2':'h1',
+        self.pos_dict = {'r1':'a1','h1':'b1','b1':'c1','q1':'d1','k':'e1','b2':'f1','h2':'g1','r2':'h1',
                         'p1':'a2','p2':'b2','p3':'c2','p4':'d2','p5':'e2','p6':'f2','p7':'g2','p8':'h2',
                         'P1':'a7','P2':'b7','P3':'c7','P4':'d7','P5':'e7','P6':'f7','P7':'g7','P8':'h7',
-                         'R1':'a8','H1':'b8','B1':'c8','Q1':'d8','K1':'e8','B2':'f8','H2':'g8','R2':'h8'}
+                         'R1':'a8','H1':'b8','B1':'c8','Q1':'d8','K':'e8','B2':'f8','H2':'g8','R2':'h8'}
 #     def board(self):
         x = ['a','b','c','d','e','f','g','h']
         y = ['1','2','3','4','5','6','7','8']
